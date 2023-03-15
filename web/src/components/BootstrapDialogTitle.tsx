626915e16b946b8b6d70d16cd61ce07392ed2c37
jradziejewski <261309@student.pwr.edu.pl>
2023-03-15 16:57:08 +0100

    add harmonogram

---
+++ web/src/components/BootstrapDialogTitle.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { DialogTitle, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+export interface DialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
+
+export function BootstrapDialogTitle(props: DialogTitleProps) {
+  const { children, onClose, ...other } = props;
+
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
+}
--- web/src/components/Main.tsx
+++ web/src/components/Main.tsx
@@ -1,21 +1,95 @@
 import React from "react";
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Dialog,
+  Grid,
+  styled,
+  Typography,
+} from "@mui/material";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import { BootstrapDialogTitle } from "./BootstrapDialogTitle";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
 
 export default function Counter({ children }: { children: JSX.Element }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Card>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              SAMI
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Smart Atmospheric Meteorological Instrument
-            </Typography>
-          </CardContent>
-        </CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            SAMI
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Smart Atmospheric Meteorological Instrument
+          </Typography>
+          <Grid
+            container
+            alignItems="center"
+            direction="column"
+            sx={{ gap: 2, marginTop: 2 }}
+          >
+            <Grid item>
+              <Button onClick={handleClickOpen}>Pokaż harmonogram</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
       </Card>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleClose}
+        >
+          Harmonogram
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            SAM Instrument Harmonogram
+            <br /> 1.
+            <br /> 2.
+            <br /> 3.
+            <br /> 4.
+            <br /> 5.
+            <br /> 6.
+            <br /> 7.
+            <br /> 8.
+            <br /> 9.
+            <br /> 10.
+            <br /> 11.
+            <br /> 12.
+            <br /> 13.
+            <br />
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Zamknij
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
     </>
   );
 }
