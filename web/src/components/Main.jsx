1331a2c8660437fc985b772fa10635905e0a910a
jradziejewski <261309@student.pwr.edu.pl>
2023-05-09 19:09:35 +0200

    add new website design
    :wq/.gitignore

---
+++ web/src/components/Main.jsx
@@ -0,0 +1,68 @@
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+} from "@mui/material";
+import Users from "./Users";
+import Sami from "../assets/sami.svg";
+import GroupIcon from "@mui/icons-material/Group";
+import StraightenIcon from "@mui/icons-material/Straighten";
+import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
+
+const drawerWidth = 240;
+
+export default function Main() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
+        <Toolbar color="primary">
+          <img src={Sami} width={30} height={30} />
+          <Box sx={{ marginLeft: 2 }}>SAMI</Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <List>
+          {listItems.map((entry) => (
+            <ListItem key={entry.text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{entry.icon}</ListItemIcon>
+                <ListItemText primary={entry.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Users />
+    </Box>
+  );
+}
+
+const listItems = [
+  { text: "Users", icon: <GroupIcon /> },
+  { text: "Measurements", icon: <StraightenIcon /> },
+  { text: "Probes", icon: <DeviceThermostatIcon /> },
+];
---
+++ web/src/customTheme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#146c94",
+    },
+    secondary: {
+      main: "#19a7ce",
+    },
+    background: {
+      default: "#f6f1f1",
+      paper: "#f6f1f1",
+    },
+    info: {
+      main: "#afd3e2",
+    },
+    error: {
+      main: "#d84315",
+    },
+  },
+});
