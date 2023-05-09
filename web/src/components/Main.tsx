2557fb3419527765dc4ba0f63746f2a771d5dca0
jradziejewski <261309@student.pwr.edu.pl>
2023-05-09 19:09:35 +0200

    add new website design

--- web/src/components/Main.tsx
+++ web/src/components/Main.tsx
@@ -1,94 +1,166 @@
-import React from "react";
-import {
-  Button,
-  Card,
-  CardContent,
-  Dialog,
-  Grid,
-  styled,
-  Typography,
-} from "@mui/material";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import { BootstrapDialogTitle } from "./BootstrapDialogTitle";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import CameraIcon from "@mui/icons-material/PhotoCamera";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
-
-export default function Counter({ children }: { children: JSX.Element }) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+function Copyright() {
   return (
-    <>
-      <Card>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            SAMI
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Smart Atmospheric Meteorological Instrument
-          </Typography>
-          <Grid
-            container
-            alignItems="center"
-            direction="column"
-            sx={{ gap: 2, marginTop: 2 }}
-          >
-            <Grid item>
-              <Button onClick={handleClickOpen}>Pokaż harmonogram</Button>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
+    <Typography variant="body2" color="text.secondary" align="center">
+      <Link
+        color="inherit"
+        href="https://github.com/jradziejewski/saminstrument"
       >
-        <BootstrapDialogTitle
-          id="customized-dialog-title"
-          onClose={handleClose}
-        >
-          Harmonogram
-        </BootstrapDialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            SAM Instrument Harmonogram
-            <br /> 1.
-            <br /> 2.
-            <br /> 3.
-            <br /> 4.
-            <br /> 5.
-            <br /> 6.
-            <br /> 7.
-            <br /> 8.
-            <br /> 9.
-            <br /> 10.
-            <br /> 11.
-            <br /> 12.
-            <br /> 13.
-            <br />
+        SAMInstrument
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const cards = [
+  {
+    id: 1,
+    img: "https://www.publichealthpost.org/wp-content/uploads/2020/07/1-1.png",
+    heading: "Humidity",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1505672678657-cc7037095e60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    heading: "Wind",
+  },
+  {
+    id: 3,
+    img: "https://cdn.mos.cms.futurecdn.net/jqkeHZrc7H2qJGzQkfkgnX.jpg",
+    heading: "Temperature",
+  },
+  {
+    id: 4,
+    img: "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1652341228/EducationHub/photos/barometer.jpg",
+    heading: "Barometric pressure",
+  },
+];
+
+const theme = createTheme();
+
+export default function Main() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            SAM Instrument
           </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Zamknij
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-    </>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              SAM Instrument
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              The Smart Atmospheric Meteorological Instrument is a powerful tool
+              that allows for accurate and real-time monitoring of atmospheric
+              conditions. With the ability to measure wind, humidity,
+              temperature, and barometric pressure and save it to a database,
+              this device is a must-have for weather stations, research
+              facilities, and other applications where precise environmental
+              data is essential.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Go to API</Button>
+              <Button variant="outlined">GITHUB</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card.id} xs={12} sm={6} md={3}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 200, height: 150 }}
+                    image={card.img}
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.heading}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    {/* <Button size="small">View</Button>
+                    <Button size="small">Edit</Button> */}
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          SAMI
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Smart Atmospheric Meteorological Instrument
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
   );
 }
